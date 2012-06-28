c314353007ae861df1541723ba0ce6b909973553
Javier Acero <j4cegu@gmail.com>
2012-06-28 16:42:18 +0200

    recognizing negative integers

--- spec/grammar/number_spec.rb
+++ spec/grammar/number_spec.rb
@@ -3,7 +3,11 @@ require 'treeton/grammar/number'
 describe_rules_for 'number' do
   it 'recognizes positive integers' do
     '1'.should be_recognized
-    '10'.should be_recognized
     '3450'.should be_recognized
+  end
+
+  it 'recognizes negative integers' do
+    '-4'.should be_recognized
+    '-8420'.should be_recognized
   end
 end
