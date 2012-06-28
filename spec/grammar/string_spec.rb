64598ad4744543b77ae3ca3ee55d6d6b31f4d6aa
Javier Acero <j4cegu@gmail.com>
2012-06-28 19:15:39 +0200

    recognizing regular characters and whole strings

--- spec/grammar/string_spec.rb
+++ spec/grammar/string_spec.rb
@@ -3,10 +3,6 @@ require 'treeton/grammar/string'
 describe_rules_for 'string' do
   let(:parser) { JsonStringParser.new }
 
-  it 'recognizes an empty string' do
-    '""'.should be_recognized
-  end
-
   context 'escaped characters' do
     it 'recognizes an escaped quotation mark' do
       %q{"\""}.should be_recognized
@@ -51,6 +47,43 @@ describe_rules_for 'string' do
 
     it 'does not recognize hexadicimal numbers with less than 4 digits' do
       %q{"\uF"}.should_not be_recognized
+    end
+  end
+
+  context 'regular characters' do
+    it 'does not recognize "\"' do
+      %q{"\\"}.should_not be_recognized
+    end
+
+    it 'does not recognize """' do
+      %q{"""}.should_not be_recognized
+    end
+
+    it 'recognizes any other unicode character' do
+      %q{"a"}.should be_recognized
+      %q{"Z"}.should be_recognized
+      %q{"5"}.should be_recognized
+    end
+  end
+
+  context 'strings' do
+    it 'recognizes an empty string' do
+      '""'.should be_recognized
+    end
+
+    it 'recognizes plain strings' do
+      %q{"This IS a PLAIN string"}.should be_recognized
+      %q{"1234567890"}.should be_recognized
+      %q{"-0.1239e10"}.should be_recognized
+      %q{"2 times TWO = four"}.should be_recognized
+    end
+
+    it 'recognizes strings with escaped characters' do
+      %q{"\tnew\n\tline\\\r\n"}.should be_recognized
+    end
+
+    it 'recognizes strings with characters by hexadecimal code' do
+      %q{"\u234F this and \u8840 is \u2343 \r\n"}.should be_recognized
     end
   end
 end
