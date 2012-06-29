911f3a76bcfdeb4a55e18520b87ad3df5a841403
Javier Acero <j4cegu@gmail.com>
2012-06-29 16:04:52 +0200

    recognizing objects with a single element

--- spec/grammar/object_spec.rb
+++ spec/grammar/object_spec.rb
@@ -7,4 +7,10 @@ describe_rules_for 'Object' do
     '{}'.should be_recognized
     ' { } '.should be_recognized
   end
+
+  it 'recognizes a object with a single element' do
+    %Q[{"key":"value" }].should be_recognized
+    %Q[{"key" : 1234 }].should be_recognized
+    %Q[ { "string"\n:\t1234 } ].should be_recognized
+  end
 end
