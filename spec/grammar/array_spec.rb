8261564ee44e9e3269d1c3fae0da580c399b2b30
Javier Acero <j4cegu@gmail.com>
2012-06-29 12:56:14 +0200

    handle white space around square brackets

--- spec/grammar/array_spec.rb
+++ spec/grammar/array_spec.rb
@@ -9,12 +9,13 @@ describe_rules_for 'Array' do
 
   it 'recognizes an array with a single value' do
     '[1024]'.should be_recognized
+    ' [ "array" ] '.should be_recognized
   end
 
   it 'recognizes multiple values' do
     '[1,2]'.should be_recognized
     '["a", "b"]'.should be_recognized
-    '["str"  ,  "ing"]'.should be_recognized
+    %Q{\n\t["str"  ,  "ing"\n\t] }.should be_recognized
     '[null, true, false, 334, "array"]'.should be_recognized
   end
 
