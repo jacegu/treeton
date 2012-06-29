b44f40c0030292404e73abb71d9ba97e645e55fb
Javier Acero <j4cegu@gmail.com>
2012-06-29 16:16:11 +0200

    recognize objects with more than one element

--- spec/grammar/object_spec.rb
+++ spec/grammar/object_spec.rb
@@ -8,9 +8,18 @@ describe_rules_for 'Object' do
     ' { } '.should be_recognized
   end
 
-  it 'recognizes a object with a single element' do
+  it 'recognizes objects with a single element' do
     %Q[{"key":"value" }].should be_recognized
     %Q[{"key" : 1234 }].should be_recognized
     %Q[ { "string"\n:\t1234 } ].should be_recognized
+  end
+
+  it 'recognizes objects with multiple elements' do
+    %Q[{"k1":"v1", "k2":"v2"}].should be_recognized
+    %Q[ { "k1":"v1" , "k2":["v1", "v2"], "k3" :234}].should be_recognized
+  end
+
+  it 'does not recognize objects with non-string keys' do
+    %Q[{ 1234:"value" }].should_not be_recognized
   end
 end
