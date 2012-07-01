9312ef59614f657186326a2ab81efaa7ca999f42
Javier Acero <j4cegu@gmail.com>
2012-07-01 20:01:32 +0200

    nodes fit in a single file

---
+++ lib/treeton/nodes.rb
@@ -0,0 +1,27 @@
+module Treeton
+
+  class NumberNode < Treetop::Runtime::SyntaxNode
+    def translate
+      eval text_value
+    end
+  end
+
+  class TrueNode < Treetop::Runtime::SyntaxNode
+    def translate
+      true
+    end
+  end
+
+  class FalseNode < Treetop::Runtime::SyntaxNode
+    def translate
+      false
+    end
+  end
+
+  class NilNode < Treetop::Runtime::SyntaxNode
+    def translate
+      nil
+    end
+  end
+
+end
--- spec/treeton/nodes_spec.rb
+++ spec/treeton/nodes_spec.rb
@@ -1,10 +1,9 @@
 require 'treeton/grammar/value'
-require 'treeton/nodes/terminal_nodes'
 
 describe 'Terminal Nodes' do
   let(:parser) { Treeton::ValueParser.new }
 
-  describe Treeton::Nodes::NumberNode do
+  describe Treeton::NumberNode do
     it 'should be translated to the number' do
       '0'.should be_translated_to 0
       '0.5e2'.should be_translated_to 50.0
@@ -12,19 +11,19 @@ describe 'Terminal Nodes' do
     end
   end
 
-  describe Treeton::Nodes::TrueNode do
+  describe Treeton::TrueNode do
     it 'should be translated to true' do
       'true'.should be_translated_to true
     end
   end
 
-  describe Treeton::Nodes::FalseNode do
+  describe Treeton::FalseNode do
     it 'should be translated to false' do
       'false'.should be_translated_to false
     end
   end
 
-  describe Treeton::Nodes::NilNode do
+  describe Treeton::NilNode do
     it 'should be translated to nil' do
       'null'.should be_translated_to nil
     end
